a15956544d49fa9445615e4e1c0dd5490ba5af5d
Mike Izbicki <mike@izbicki.me>
2021-02-04 23:43:07 -0800

    update tests

--- tests/test_ec.py
+++ tests/test_ec.py
@@ -11,11 +11,14 @@ def test__extract_tags_2():
         HTML_Validator._extract_tags('this is a <strong test')
 
 def test__extract_tags_3():
-    assert HTML_Validator._extract_tags('this is a <strong test>') == ['<strong test>']
-
-def test__extract_tags_4():
     with pytest.raises(ValueError, match='found < without matching >'):
         HTML_Validator._extract_tags('this is a <strong< test')
+
+def test__extract_tags_4():
+    assert HTML_Validator._extract_tags('this is a <strong test>') == ['<strong>']
+
+def test__extract_tags_4():
+    assert HTML_Validator._extract_tags('this is a <a href="https://izbicki.me">link</a>') == ['<a>','</a>']
 
 
 def test_validate_html_1():
@@ -25,7 +28,10 @@ def test_validate_html_2():
     assert not HTML_Validator.validate_html('this is a <strong test')
 
 def test_validate_html_3():
-    assert not HTML_Validator.validate_html('this is a <strong test>') == ['<strong test>']
+    assert not HTML_Validator.validate_html('this is a <strong test>')
+
+def test_validate_html_3():
+    assert HTML_Validator.validate_html('this is a <strong test> bold me </strong>')
 
 def test_validate_html_4():
     assert not HTML_Validator.validate_html('this is a <strong< test')
